f05d13fa68122b8dd2f103701c3a9c3cf8745774
Tim Hua <Tim@Tims-MBP.local>
2015-03-03 15:28:23 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var unirest = require('unirest');
+var moment = require('moment');
+var app = express();
+var now = moment.utc().toISOString();
+var start =  moment.utc().subtract(1, 'hours').toISOString()
+var port = 3000;
+var result
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+  res.send('index.html');
+  res.end();
+});
+
+app.get('/api/getData', function(req,res){
+  var query = { 
+              "queryType": "timeseries",
+              "dataSource": "points_100ms",
+              "granularity": {"type": "period", "period": "PT1s"},
+              "aggregations":[ {"type": "count", "name": "rowCount"} ],
+              "intervals": [moment.utc().subtract(1, 'hours').toISOString() + '/' + moment.utc().toISOString()]
+            }
+  unirest.post('http://192.168.20.220:2179/druid/v2/?')
+    .header( { 'content-type':'application/json' })
+    .send(query)
+    .end(function(response){
+      console.log('db called');
+      result = response;
+      console.log('done');
+    });
+  res.send(result);
+  res.end();
+});
+
+app.listen(port);
+console.log('listening on', port);
---
+++ public/javascript.js
@@ -0,0 +1,66 @@
+  $(function() {
+    var results = {};
+    var sortedResults = [];
+    var snapshot = [];
+    var html = '';
+    var color;
+    var highCount;
+    var badCount = [];
+    var minCount = 100;
+    var date = new Date()
+    $('#time').html("<h5>Last page refresh PST time: " + date +"</h5>");
+    $.getJSON('/api/getData', function(data){
+      // console.log(data.body);
+      
+
+      data.body.forEach(function(el){
+         if(el.result.rowCount in results) {
+          results[el.result.rowCount][0]++;
+        } else {
+          results[el.result.rowCount] = [1, el.timeStamp];
+        }
+      })
+      for(var el in results){
+        sortedResults.push([el, results[el]])
+      }
+      sortedResults.sort(function(a,b){return b[1][0]-a[1][0];})
+      highCount = sortedResults[0][0];
+      console.log(highCount);
+      for(var i=0; i < sortedResults.length;i++){
+        $('#containertable').append("<tr><td><p>" + sortedResults[i][0] + " : " + sortedResults[i][1] + "</p></td></tr>");
+      }
+      if(data.body.length < minCount){
+        minCount = data.body.length-1;
+      }
+      for(var i = data.body.length-1; i > 0; i--){
+        var tsDate = new Date(data.body[i].timestamp)
+        // console.log(tsDate);
+        if(data.body[i].result.rowCount == highCount){
+          color = "";
+        } else {
+          color = "red";
+          badCount.push([tsDate, data.body[i].result.rowCount])
+        }
+        html += '<tr><td bgcolor="'+ color +'" name="'+ data.body[i].timestamp +'"">' + tsDate + ' - '+ data.body[i].result.rowCount + '</td></tr>';
+      }
+      if(badCount !== []){
+        var badCountHTML = '';
+        
+        for(var i = 0; i< badCount.length; i++){
+          badCountHTML += '<tr><td><p>' + badCount[i][0] + ' - ' + badCount[i][1] + '</p></td></tr>';
+        }
+        $('#badcounttable').html(badCountHTML);
+      }
+      var summary = {
+        total: data.body.length,
+        highCount: highCount,
+        badCount: badCount.length,
+      }
+      $('#summarytable').html('<tr><td><p> Total Records: ' + data.body.length + '</p></td></tr>' +
+                              '<tr><td><p> Total time: ' + Math.floor(data.body.length/60) + ' min</p></td></tr>' +
+                              '<tr><td><p> Target Record Count: ' + highCount + '</p></td></tr>' + 
+                              '<tr><td><p> Bad Record Count: ' + badCount.length + '</p></td></tr>');
+      $('#snapshottable').html(html);
+    });
+  });
+
